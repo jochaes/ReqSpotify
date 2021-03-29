2f53d1d493ddc8dcc0f65689993119b19e78f106
Josué <d4n11083@estudiantec.cr>
2021-03-28 22:04:18 -0600

    Nuevas clases para conectarse al API y clases para guardar info de User y Song

---
+++ app/src/main/java/Connectors/SongService.java
@@ -0,0 +1,132 @@
+package Connectors;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.reqspotify.Song;
+
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Album;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class SongService {
+    private ArrayList<Song> songs = new ArrayList<>();
+    private SharedPreferences sharedPreferences;
+    private RequestQueue queue;
+
+    public SongService(Context context) {
+        sharedPreferences = context.getSharedPreferences("SPOTIFY", 0);
+        queue = Volley.newRequestQueue(context);
+    }
+
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
+
+    public ArrayList<Song> getRecentlyPlayedTracks(final VolleyCallback callBack) {
+        String endpoint = "https://api.spotify.com/v1/me/player/recently-played";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, endpoint, null, response -> {
+                    Gson gson = new Gson();
+                    JSONArray jsonArray = response.optJSONArray("items");
+                    for (int n = 0; n < jsonArray.length(); n++) {
+                        try {
+                            JSONObject object = jsonArray.getJSONObject(n);
+                            object = object.optJSONObject("track");
+                            Song song = gson.fromJson(object.toString(), Song.class);
+                            songs.add(song);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    callBack.onSuccess();
+                }, error -> {
+                    // TODO: Handle error
+
+                }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                String token = sharedPreferences.getString("token", "");
+                String auth = "Bearer " + token;
+                headers.put("Authorization", auth);
+                return headers;
+            }
+        };
+        queue.add(jsonObjectRequest);
+        return songs;
+    }
+
+    public void addSongToLibrary(Song song) {
+        JSONObject payload = preparePutPayload(song);
+        JsonObjectRequest jsonObjectRequest = prepareSongLibraryRequest(payload);
+        queue.add(jsonObjectRequest);
+    }
+
+    private JsonObjectRequest prepareSongLibraryRequest(JSONObject payload) {
+        return new JsonObjectRequest(Request.Method.PUT, "https://api.spotify.com/v1/me/tracks", payload, response -> {
+        }, error -> {
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                String token = sharedPreferences.getString("token", "");
+                String auth = "Bearer " + token;
+                headers.put("Authorization", auth);
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+        };
+    }
+
+    private JSONObject preparePutPayload(Song song) {
+        JSONArray idarray = new JSONArray();
+        idarray.put(song.getId());
+        JSONObject ids = new JSONObject();
+        try {
+            ids.put("ids", idarray);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return ids;
+    }
+
+    ///Esto es super importante omg si sirvió jejeps
+    public void getTracks() {
+        SpotifyApi api = new SpotifyApi();
+        api.setAccessToken(sharedPreferences.getString("token", ""));
+
+        SpotifyService spotify = api.getService();
+        spotify.getAlbum("1GbtB4zTqAsyfZEsm1RZfx", new Callback<Album>() {
+            @Override
+            public void success(Album album, Response response) {
+                Log.d("Album success", album.name);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.d("Album failure", error.toString());
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/Connectors/VolleyCallback.java
@@ -0,0 +1,6 @@
+package Connectors;
+
+public interface VolleyCallback {
+
+    void onSuccess();
+}
